ece37617022ef10d0c839ab88677737d00711439
LeoniePeters <leoniepeters@gmail.com>
2019-01-09 16:13:06 +0100

    Set Up Form Validation

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -6,6 +6,9 @@ function submitComment () {
     const comment = document.createElement('section')
     const h3 = document.createElement('h3')
     const p = document.createElement('p')
+    if (doesNotPassAllValidations(name,msg)){
+        return null
+    }
     h3.innerHTML = `${name} said:`
     p.innerHTML = msg
     comment.classList.add('comment')
@@ -15,4 +18,26 @@ function submitComment () {
     commentSection.appendChild(comment)
     inputField.value = null
     textArea.value = null
+}
+
+function doesNotPassAllValidations (name,msg) {
+    if(!name && !msg){
+        alert('You forgot to fill in your name and message!')
+        return true
+    } 
+    if(!name){
+        alert('You forgot to fill in your name!')
+        return true
+    } 
+    if(!msg){
+        alert('You forgot to fill in your message!')
+        return true
+    } 
+
+    if (msg.length > 280) {
+        alert('Your comment is too long! Max. 280 characters')
+        return true
+    } 
+
+    return false
 }
